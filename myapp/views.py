a1d886a9f6b73cf1adab56028bde68cf3252416b
begimaiSydykova <bsydykova0712@gmail.com>
2023-12-13 15:14:26 +0600

    1/14 : Added category page

---
+++ myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Category, Product
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'category_list.html', {'categories': categories})
+
+def category_detail(request, category_id):
+    category = Category.objects.get(id=category_id)
+    products = Product.objects.filter(category=category)
+    return render(request, 'category_detail.html', {'category': category, 'products': products})
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
